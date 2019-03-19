3e57bac2df985bfe53f1a5da05cb109ef0a56d9c
Matthew Hamilton <matthewhamilton@client.openvpn.net>
2019-03-19 16:41:05 -0400

    Initial Commit

---
+++ SonarQubeObjCTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SonarQubeObjCTest
+//
+//  Created by Matthew Hamilton on 3/19/19.
+//  Copyright © 2019 KISS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ SonarQubeObjCTestTests/SonarQubeObjCTestTests.m
@@ -0,0 +1,37 @@
+//
+//  SonarQubeObjCTestTests.m
+//  SonarQubeObjCTestTests
+//
+//  Created by Matthew Hamilton on 3/19/19.
+//  Copyright © 2019 KISS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SonarQubeObjCTestTests : XCTestCase
+
+@end
+
+@implementation SonarQubeObjCTestTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
